57528f705628e64ea1bcc836327cbf37c5bb055a
“oleksandr” <alexfil19998@gmail.com>
2025-05-21 00:22:57 +0200

    added task1,2,3,4 for homework 12_05

---
+++ src/homework20_05/Task1/PizzaCalories.java
@@ -0,0 +1,24 @@
+package homework20_05.Task1;
+
+public class PizzaCalories {
+
+    public static double calculateCalories(int diameter) {
+        double radius = diameter / 2.0;
+        double area = Math.PI * radius * radius;
+        return area * 40;
+    }
+
+    public static void main(String[] args) {
+        int diameter1 = 24;
+        int diameter2 = 28;
+
+        double calories1 = calculateCalories(diameter1);
+        double calories2 = calculateCalories(diameter2);
+
+        double extraCalories = calories2 - calories1;
+
+        System.out.println("Калорий в пицце 24 см: " + (int)calories1);
+        System.out.println("Калорий в пицце 28 см: " + (int)calories2);
+        System.out.println("Лишние калории: " + (int)extraCalories);
+    }
+}
